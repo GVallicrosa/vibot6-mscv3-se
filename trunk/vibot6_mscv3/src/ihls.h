84a151e3407ea17d3650355eb3708e006b1f8166
akbarinia.arash@gmail.com <akbarinia.arash@gmail.com@711acd62-a9e1-c373-c03c-dfc2232d3d9e>
2011-11-13 19:46:41 +0000

    Header for ihls.

--- trunk/vibot6_mscv3/src/ihls.h
+++ trunk/vibot6_mscv3/src/ihls.h
@@ -2,10 +2,20 @@
  * ihls.h
  *
  * Header file for IHLS (Improved Hue Luminance Saturation)
+ *
+ * The main function is convert_rgb_to_ihls which must be called
+ * from outside. The rest of functions are provided publicly if
+ * someone wants to directly retrieve hue, saturation or luminance
+ * based on RGB values.
+ *
  */
 
 #ifndef IHLS_H_
 #define IHLS_H_
+
+#include <cv.h>
+
+using namespace cv;
 
 /**
  * Returning hue value of RGB.
@@ -24,5 +34,19 @@ retrieve_luminance(unsigned int r, unsigned int g, unsigned int b);
  */
 float
 retrieve_saturation(unsigned int r, unsigned int g, unsigned int b);
+
+/**
+ * This function converts the an RGB image to IHLS and returns it.
+ *
+ * The reason that we have a return matrix and not overriding the original
+ * RGB image, is to keep the original RGB values for reference.
+ *
+ * FIXME: both rgb_image and the returned IHLS images are taken as
+ * two dimensional matrices of floating points. This is the starting
+ * prototype and can be changed later on to some IMAGE types from
+ * some libraries (like OpenCV ...).
+ */
+Mat
+convert_rgb_to_ihls(Mat rgb_image);
 
 #endif /* IHLS_H_ */
