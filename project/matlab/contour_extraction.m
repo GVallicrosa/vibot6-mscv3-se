e52e96a493164f64ee8ebbfcfe192ed03624a670
guillemvallicrosa@gmail.com <guillemvallicrosa@gmail.com@711acd62-a9e1-c373-c03c-dfc2232d3d9e>
2011-12-04 16:50:21 +0000

    Moved code from Postprocessing to ContourExtraction. Created a sample main file.

--- project/matlab/contour_extraction.m
+++ project/matlab/contour_extraction.m
@@ -1,4 +1,4 @@
-function valcont = contour_extraction(contP, convHull, distanceError)
+function valid = contour_extraction(contP, distanceError)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%         Contour extraction         %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -11,61 +11,56 @@ function valcont = contour_extraction(contP, convHull, distanceError)
 %   Muhammed Safiul Azam
 %
 %   ---Inputs---
-%   contP          : structure containing contour points of each region
-%                    in a image sorted CCW
-%   convHull       : structure containing the indexs of the contour
-%                    points that are in the convex hull
+%   contP          : contour points of the region of an image sorted CCW
 %   distanceError  : parameter to set the maximum distance to the convex
 %                    hull polygon to take the points
 %
 %   ---Outputs---
-%   valcont        : structure containing all the valid points at a
-%                    distance less or equal to 'distanceError' of the
-%                    convex hull polygon
+%   valid          : valid points at a distance less or equal to
+%                    'distanceError' of the convex hull polygon
 %
 %   ---Testing---
 %   TODO
 
-for n=1:length(contP)
-    %% Data for each region
-    valid = [];          % variable to save near points
-    points = contP{n};   % points of current contour
-    K = convHull{n};     % indexs of points in convex hull, in current contour
-    allx = points(:,1);  % x positions for all points
-    ally = points(:,2);  % y positions for all points
+%% Convex hull
+% Obtaining the points in the segmented region that correspond to the CH
+convHull = convhull(contP(:,1),contP(:,2)); % indexs of points in convex hull, in current contour
+
+%% Data for the region
+valid = [];          % variable to save near points
+allx = contP(:,1);   % x positions for all points
+ally = contP(:,2);   % y positions for all points
     
-    for i=1:(length(K)-1)
-        %% Segment of convex hull polygon to check
-        % convex hull indexs
-        ko = K(i);
-        kf = K(i+1);
-        % convex hull points
-        xo = allx(ko);
-        yo = ally(ko);
-        xf = allx(kf);
-        yf = ally(kf);
-        
-        %% Contour points to check
-        if (kf>ko)
-            xs = allx(ko:kf-1);
-            ys = ally(ko:kf-1);
-        else
-            xs = [allx(1:kf-1) allx(ko:length(allx))];
-            ys = [ally(1:kf-1) ally(ko:length(ally))];
-        end
-        
-        %% Check all the points distance to line
-        for j = 1:length(xs)
-            % obtain point
-            xp = xs(j);
-            yp = ys(j);
-            % obtain distance
-            dist = abs((xf-xo)*(yp-yo)-(yf-yo)*(xp-xo))/sqrt((xf-xo)^2+(yf-yo)^2);
-            % if dist < error, include point
-            if (abs(dist) < distanceError)
-                valid = [valid; xp, yp];
-            end
+for i=1:(length(convHull)-1)
+    %% Segment of convex hull polygon to check
+    % convex hull indexs
+    ko = convHull(i);
+    kf = convHull(i+1);
+    % convex hull points
+    xo = allx(ko);
+    yo = ally(ko);
+    xf = allx(kf);
+    yf = ally(kf);
+
+    %% Contour points to check
+    if (kf>ko)
+        xs = allx(ko:kf-1);
+        ys = ally(ko:kf-1);
+    else
+        xs = [allx(1:kf-1) allx(ko:length(allx))];
+        ys = [ally(1:kf-1) ally(ko:length(ally))];
+    end
+
+    %% Check all the points distance to line
+    for j = 1:length(xs)
+        % obtain point
+        xp = xs(j);
+        yp = ys(j);
+        % obtain distance
+        dist = abs((xf-xo)*(yp-yo)-(yf-yo)*(xp-xo))/sqrt((xf-xo)^2+(yf-yo)^2);
+        % if dist < error, include point
+        if (abs(dist) < distanceError)
+            valid = [valid; xp, yp];
         end
     end
-    valcont{n} = valid;
 end
