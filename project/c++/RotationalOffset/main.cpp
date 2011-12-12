d437970dcba5e9624cb2cca9d1ef51caf0164cf6
rgilrodr7@gmail.com <rgilrodr7@gmail.com@711acd62-a9e1-c373-c03c-dfc2232d3d9e>
2011-12-12 16:12:04 +0000

    Updated Rotational Offset part.

--- project/c++/RotationalOffset/main.cpp
+++ project/c++/RotationalOffset/main.cpp
@@ -1,13 +1,78 @@
+// Only for testing
+
 #include "RotationalOffset.h"
+
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main(void)
 {
-	Contour C[5] ={{1,2,5},{1,5,5},{50,97,5},{1,31,5},{20,87,5}} ;
+  ifstream fin;
+  ofstream fon("offsetO.txt");
+  string dir, filepath;
+
+  DIR *dp;
+  struct dirent *dirp;
+  struct stat filestat;
+  int count = 0;
+  float item, cost;
+
+  vector< vector< pair<unsigned int, unsigned int> > > Contours(cMAX_POINTS);
+
+  cout << "dir to get files of: " << flush;
+  getline( cin, dir );  // gets everything the user ENTERs
+
+  dp = opendir( dir.c_str() );
+  if (dp == NULL)
+    {
+    cout << "Error(" << errno << ") opening " << dir << endl;
+    return errno;
+    }
+
+  while ((dirp = readdir( dp )))
+  {
+    filepath = dir + "/" + dirp->d_name;
+
+    // If the file is a directory (or is in some way invalid) we'll skip it
+    if (stat( filepath.c_str(), &filestat )) continue;
+    if (S_ISDIR( filestat.st_mode ))         continue;
+
+
+    fin.open( filepath.c_str() );
+    while ( !fin.eof() ) {
+        fin >> item >>  cost;
+        Contours[count].push_back(make_pair((unsigned int)item,(unsigned int)cost));
+    }
+    count++;
+    item=0;
+    cost=0;
+    fin.close();
+   }
+
+	vector< vector<float> > Offset;
+
 	IRO *RO = new cRotationalOffset();
 
-	RO->GetMinRadius(C);
-	
-	system("pause");
+	Offset = RO->GetMinRadius(Contours);
 
+	// Write Offset in OffsetO.text file //
+    for(int j=0;j< cMAX_POINTS;j++){
+
+        vector<float>::iterator it2;
+
+        for(it2 = Offset[j].begin();it2 != Offset[j].end();it2++){
+            fon << *it2 << "\t";
+        }
+        fon << endl;
+    }
+
+
+
+	system("pause");
+    closedir( dp );
+    fon.close( );
 	return(0);
 }
+
