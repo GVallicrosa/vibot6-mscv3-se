899872f8fffe78943ed735a9d1b104127d74f1b1
guillemvallicrosa@gmail.com <guillemvallicrosa@gmail.com@711acd62-a9e1-c373-c03c-dfc2232d3d9e>
2011-12-27 17:23:37 +0000

    Added 'color' option for segmentation in 'normalize_segmentation.m'. Cleaned the code.

--- trunk/project/matlab/normalize_segmentation.m
+++ trunk/project/matlab/normalize_segmentation.m
@@ -1,23 +1,29 @@
-function [In] = normalize_segmentation(I)
+function [In] = normalize_segmentation(I, color)
+% Converts the image to IHLS and does the segmentation
+%
+% INPUT
+%    I      : image to process
+%    color  : 'blue' or 'red'
+%
+% OUTPUT
+%    In     : normalized and segmented image
+
 %% Convert to IHLS and normalize hue-saturation
+I2 = rgb2ihls(I);       % convert to IHLS
 
-I2 = rgb2ihls(I);
+h = I2(:,:,1);          % take hue channel
+h_norm = h*255/(2*pi);  % rearrange from 0:2*pi to 0:255
+h_norm = uint8(h_norm); % convert to uint8
 
-h = I2(:,:,1);
-h_norm = h*255/(2*pi);
-h_norm = uint8(h_norm);
+s = I2(:,:,3);          % take saturation channel
+s_norm = uint8(s);      % convert to uint8
 
-s = I2(:,:,3);
-s_norm = uint8(s);
-
-%% Color Segmentation - rough thresholding
-% red = (s_norm(i,j)>30 && h_norm(i,j)<11) || (s_norm(i,j)>30 && h_norm(i,j)>230) 
-% blue = (h_norm(i,j)>=134 && h_norm(i,j)<163) && (s_norm(i,j)>39 && s_norm(i,j)<=215) %blue = h_norm(i,j)>=140 && h_norm(i,j)<160 && s_norm(i,j)>111
-
-In = zeros(size(I,1),size(I,2));
-%% BLUE
-%p = find((h_norm>=134 & h_norm<163) & (s_norm>39 & s_norm<=215));
-%% RED
-p = find(s_norm>30 & h_norm>230 | s_norm>30 & h_norm<11);
-In(p) = 1;
+%% Color Segmentation
+In = zeros(size(I,1), size(I,2)); % create image to store output
+if strcmp(color, 'blue')
+    p = find((h_norm>=134 & h_norm<163) & (s_norm>39 & s_norm<=215));
+elseif strcmp(color, 'red')
+    p = find(s_norm>30 & h_norm>230 | s_norm>30 & h_norm<11);
+end
+In(p) = 1; % write found pixels
 
