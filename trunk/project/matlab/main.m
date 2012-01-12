1fba403a42512bba0ca2fab73a8e7a948d4fb9f5
yukti.buzz@gmail.com <yukti.buzz@gmail.com@711acd62-a9e1-c373-c03c-dfc2232d3d9e>
2012-01-12 04:07:01 +0000

    changed the CE_distError to 7 instead of sqrt(2), line 21 of main.m, for better contour extraction. Must chk for more images.

--- trunk/project/matlab/main.m
+++ trunk/project/matlab/main.m
@@ -8,9 +8,13 @@ close all;
 clear all;
 clc;
 
+%%Just for checking-Yukti
+global save_m save_rot;
+
 %% Options
 % NHS
 NHS_color  = 'red';      % color to segment 'blue' or 'red'
+
 NHS_output = true;       % save output for this function
 % PostProcessing
 POST_aspectArea = 1500;  % imageArea/aspectArea thresholding (default 1500)
@@ -18,7 +22,7 @@ POST_lowRatio   = 0.25;  % height/width lowest value (default 0.25)
 POST_highRatio  = 1.3;   % height/width highest value (default 1.3)
 POST_output     = true;  % save output for this function
 % Contour extraction
-CE_distError = sqrt(2);  % maximum distance from the convex hull to take contour pixels (default sqrt(2))
+CE_distError = 7;  % maximum distance from the convex hull to take contour pixels (default sqrt(2))
 CE_output    = true;     % save output for this function
 % Gielis reconstruction
 GIELIS_norm = false;      % use normalization (default true)
@@ -40,7 +44,7 @@ fileIndex = find(~[files.isdir]);
 %for i = 1:length(fileIndex)
     
     %% Open image
-    fname = 'octogonal0018.jpg';
+    fname = 'triangular0017.jpg';
     %fname = files(fileIndex(i)).name;
     im = imread([dirname,fname]);
     display(['- ', fname]);
@@ -70,9 +74,13 @@ fileIndex = find(~[files.isdir]);
         display(['  -- contour ', num2str(i),'/',num2str(N)])
         [rows cols] = find(L==i);              %Get the coordinates of the points
         tmpI = (L==i);                         %Make a temporary binary image of the current label
+        
+
         contP = bwtraceboundary(tmpI, [rows(1),cols(1)], 'E', 8, Inf, 'counterclockwise');
                                                %Get the points in the boundary of the blob
-                                           
+%          img_canny= edge(tmpI,'canny');
+%          contP=find(img_canny==1);
+         
         %% Contour extraction
         valid_contour = contour_extraction(contP, CE_distError);
         if CE_output
@@ -90,7 +98,7 @@ fileIndex = find(~[files.isdir]);
         [Theta,Permutation_Index] = sort(Theta,'ascend');
         Radius = Radius(Permutation_Index);
         Offset = FindMinimum(Radius, Theta);
-        
+
         %% Gielis curves reconstruction
         Output = ShapeReconstruction(valid_contour, Offset, GIELIS_norm, GIELIS_func);
         if GIELIS_output
@@ -102,7 +110,7 @@ fileIndex = find(~[files.isdir]);
         f = figure('visible','off'); 
         subplot('position', [0 0 1 1]);
         imshow(im); hold on;
-        plot(Output(:,2),Output(:,1),'g','LineWidth', 3); hold off;
+        plot(Output(:,2),Output(:,1),'g','LineWidth', 2); hold off;
         dpi = 100;
         set(f, 'paperposition', [0 0 n/dpi m/dpi]);
         set(f, 'papersize', [n/dpi m/dpi]);
